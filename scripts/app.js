c5f4245565a681bf9b7609b87f6a65d4dcd30d71
Heather Stone <heather.blair.stone@gmail.com>
2017-03-04 19:55:37 -0700

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+(function(){
+  "use strict";
+  angular
+    .module("ngClassifieds", ["ngMaterial"])
+    .config(function($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('orange');
+    })
+    .directive("helloWorld", function() {
+      return {
+        template: "<h1>{{ message }}</h1>"
+      }
+    });
+})();
